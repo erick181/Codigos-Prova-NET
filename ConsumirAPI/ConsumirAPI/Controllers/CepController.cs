f6259409709e434a8129aad4a21ab2eb65afc515
erick181 <117037035+erick181@users.noreply.github.com>
2022-10-31 17:05:15 -0300

    Consumir APi com RestSharp

---
+++ ConsumirAPI/ConsumirAPI/Controllers/CepController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+using RestSharp.Serializers;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+
+namespace ConsumirAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CepController : ControllerBase
+    {
+     
+    [HttpPost("cep")] 
+    public async Task<ActionResult> BuscarCep(String cep)
+        {
+            var client = new RestClient($"http://viacep.com.br/ws/{cep}/json/");
+
+            var request = new RestRequest("", Method.Get);
+
+            var response = await client.ExecuteAsync(request);
+
+            return Ok(response.Content);
+        }
+
+
+
+
+    }
+}
